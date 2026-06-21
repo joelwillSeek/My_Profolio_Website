9e8198980e839637f3f18efaf122209b49acf6f2
joel <joelthousend@gmail.com>
2026-06-21 14:54:25 +0300

    Remove .next from tracking

--- .next/build/e0ecff886de46119.js
+++
@@ -1,6 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("e0ecff886de46119.js")
-R.c("chunks/[turbopack-node]_transforms_postcss_ts_1wjn0nd._.js")
-R.c("chunks/[root-of-the-server]__0n4id39._.js")
-R.m("[turbopack-node]/child_process/globals.ts [postcss] (ecmascript)")
-R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/Active Projects/My_Profolio_Website/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
-module.exports=R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/Active Projects/My_Profolio_Website/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
